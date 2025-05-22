4cb7ac3700e8e94db9413bf1c87e19e15285314f
Paco De Luis <ponchoflores464@gmail.com>
2025-05-22 12:35:00 -0600

    Footer and Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="row justify-content-center justify-content-md-between">Bienvenidos al Team Pquis</div>
+  )
+}
