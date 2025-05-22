0240f051fd208bb2495ad1d810f0bb615802a899
Paco De Luis <ponchoflores464@gmail.com>
2025-05-22 12:49:44 -0600

    02

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+export default function Footer(){
+    return (
+        <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">Paquis - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+    )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
 export default function Home() {
   return (
-    <div className="row justify-content-center justify-content-md-between">Bienvenidos al Team Pquis</div>
-  )
+    <section className="flex flex-col items-center justify-center text-center min-h-screen bg-gradient-to-br from-purple-800 to-indigo-900 text-white px-4">
+      <h1 className="text-4xl md:text-5xl font-bold mb-6 drop-shadow-lg">
+        Bienvenidos al <span className="text-yellow-300">Team Pquis</span>
+      </h1>
+      <p className="text-lg md:text-xl max-w-xl font-light">
+        Vamos por la 16
+      </p>
+    </section>
+  );
 }
